ee29c38729531db5e488cba4cd45700ec08d638d
Alexey Kalashnikov <168136189+173Alex@users.noreply.github.com>
2024-12-18 01:45:29 +0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "173Alex"
+title = "Список дел"
+content = "Выполнить задание"
+status = "Не выполнено"
+create_date = "18.12.24"
+issue_date = "20.12.24"
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", create_date)
+print("Дата истечения заметки:", issue_date)
